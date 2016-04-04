c6a502e3d41eb52ce3124e8048f80f34639ffabd
Alexander Ar-Staelin <alexander2212@gmail.com>
2016-04-04 10:30:36 +0300

    added more structure to the tree

--- BuchiDeterminization/DeterminizationTree.java
+++ BuchiDeterminization/DeterminizationTree.java
@@ -6,15 +6,55 @@ public class DeterminizationTree {
  * 
  */
 	
-	//TODO
+	private Buchi buchi;
+	private List<TreeNode> nodelist;
+	//TODO add other relevant fields
 	
+	/**
+	 * Constructor for Tree.
+	 * @param buchi - the NBA to be determinized
+	 */
 	public DeterminizationTree(Buchi buchi){
 		//TODO
 	}
 	
+	/**
+	 * Does one step on the tree, with char s.
+	 * @param s - the input char
+	 * @return number for numbered automata
+	 */
 	public int doStep(char s){
 		//TODO
 	}
 	
-	//TODO getStateArray
+	/**
+	 * Returns the array representing the current state
+	 * @return Array representing tree shape
+	 */
+	public int[] getTreeStateArray(){
+		//TODO
+	}
+	
+	/**
+	 * Returns an array that represents the node that each
+	 * state belongs to.
+	 * @return Array of the numbers of the states
+	 */
+	public int[] getStateNodesArray(){
+		//TODO
+	}
+	
+	private class TreeNode{
+		public Set<BuchiState> states; //represents states of current node
+		public List<TreeNode> children;
+		public TreeNode parent;
+		public int num;
+		
+		public TreeNode(Set<BuchiState> states, TreeNode parent){
+			this.states = states;
+			this.parent = parent;
+		}
+		
+		//TODO overide equals and hash
+	}
 }
