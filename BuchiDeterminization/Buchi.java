19f2e4afef4b46d679c90865b18e32965b729385
Akmoilt <akmoilt@gmail.com>
2016-04-09 11:22:49 +0300

    Made Buchi accept generic input stream, added main to read from stdin

--- BuchiDeterminization/Buchi.java
+++ BuchiDeterminization/Buchi.java
@@ -1,5 +1,10 @@
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.regex.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 /**
  * This class represents a non-deterministic Buchi automaton (NBA).
@@ -11,36 +16,45 @@ class Buchi {
     /**
      * Interprets a graphviz graph into an automaton.
      */
-    public Buchi(String graphviz) {
+    public Buchi(InputStream in) {
         Pattern statePattern = Pattern.compile("^(?<id>\\w+)\\s\\[label=\"(?<starting>\\*?)(?<labelid>)(?<final>\\$?)\"\\]$");
         Pattern transitionPattern = Pattern.compile("^\t(?<startID>\\w+)\\s->\\s(?<nextID>\\w+)\\s\\[label=(?<character>\\w)\\]$");
-        
-        Scanner scanner  = new Scanner(graphviz);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            Matcher stateMatcher = statePattern.matcher(line);
-            if (stateMatcher.matches()) {
-                states.put(stateMatcher.group("id"), new BuchiState(stateMatcher.group("id"), stateMatcher.group("starting") == "*",
-                        stateMatcher.group("final") == "$"));
-                continue;
-            }
-            Matcher transitionMatcher = transitionPattern.matcher(line);
-            if (transitionMatcher.matches()) {
-                BuchiState state = states.get(transitionMatcher.group("startID"));
-                Character nextChar = transitionMatcher.group("character").charAt(0);
-                if (state.transitions.containsKey(nextChar)) {
-                    state.transitions.get(nextChar).add(transitionMatcher.group("nextID"));
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            for (String line : reader.lines().collect(Collectors.toList())) {
+                Matcher stateMatcher = statePattern.matcher(line);
+                if (stateMatcher.matches()) {
+                    states.put(stateMatcher.group("id"), new BuchiState(stateMatcher.group("id"), stateMatcher.group("starting") == "*",
+                                stateMatcher.group("final") == "$"));
+                    continue;
+                }
+                Matcher transitionMatcher = transitionPattern.matcher(line);
+                if (transitionMatcher.matches()) {
+                    BuchiState state = states.get(transitionMatcher.group("startID"));
+                    Character nextChar = transitionMatcher.group("character").charAt(0);
+                    if (state.transitions.containsKey(nextChar)) {
+                        state.transitions.get(nextChar).add(transitionMatcher.group("nextID"));
+                    }
+                    else {
+                        // convert a single element to an array as ugly hack to initialize set with value
+                        state.transitions.put(nextChar, new HashSet<String>(Arrays.asList(transitionMatcher.group("nextID"))));
+                    }
+                    continue;
                 }
                 else {
-                    // convert a single element to an array as ugly hack to initialize set with value
-                    state.transitions.put(nextChar, new HashSet<String>(Arrays.asList(transitionMatcher.group("nextID"))));
+                    System.out.println("Invalid input format.");
                 }
-                continue;
-            }
-            else {
-                System.out.println("Invalid input format.");
             }
         }
-        scanner.close();
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Reads NBA in graphviz format from stdin, prints the resulting DNA, again in graphviz format
+     */
+    public static void main(String[] args) {
+        System.out.println(new Buchi(System.in)); // TODO call determinization function, move this to new class
     }
 }
