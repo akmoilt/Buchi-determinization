178efa79b90e1055e3ceb86f7400a466eb9f150a
Alexander Ar-Staelin <alexander2212@gmail.com>
2016-04-27 12:48:19 +0300

    Added new project for non emptiness

---
+++ NonEmptiness/src/NumberedAnalyzer.java
@@ -0,0 +1,17 @@
+
+public class NumberedAnalyzer {
+
+	/**
+	 * This class is an analyzer for numbered parity automata.
+	 * 
+	 */
+	
+	// TODO private fields
+	
+	// public NumberedAnalyzer(Stream input)
+	
+	// public boolean isNonEmpty()
+	
+	// private dfs(int number ??) ??
+	
+}
