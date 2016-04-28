3b66afbad88a27404dec3fabe01532967e016f33
Akmoilt <akmoilt@gmail.com>
2016-04-28 13:00:07 +0300

    Added classes representing DNA + graphviz constructor

--- NonEmptiness/src/NumberedAnalyzer.java
+++ NonEmptiness/src/NumberedAnalyzer.java
@@ -1,17 +1,23 @@
-
+/**
+ * This class is an analyzer for numbered parity automata.
+ */
 public class NumberedAnalyzer {
 
-	/**
-	 * This class is an analyzer for numbered parity automata.
-	 * 
-	 */
-	
 	// TODO private fields
 	
-	// public NumberedAnalyzer(Stream input)
+	public NumberedAnalyzer(Numbered numbered)
+    {
+
+    }
 	
-	// public boolean isNonEmpty()
-	
-	// private dfs(int number ??) ??
-	
+    public boolean isEmpty()
+    {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Numbered numbered = new Numbered(System.in);
+        NumberedAnalyzer analyzer = new NumberedAnalyzer(numbered);
+        System.out.println(!analyzer.isEmpty());
+    }
 }
---
+++ NonEmptiness/src/NumberedTransition.java
@@ -0,0 +1,9 @@
+public class NumberedTransition {
+    public final String id;
+    public final int transitionNumber;
+
+    public NumberedTransition(String id, int transitionNumber) {
+        this.id = id;
+        this.transitionNumber = transitionNumber;
+    }
+}
